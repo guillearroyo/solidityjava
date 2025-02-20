3906ed063112e6ec9dd59d90cdfbd32332276afd
guillearroyo <guillermoizquierdoarroyo@gmail.com>
2025-02-20 17:46:46 +0100

    david <3 quevedo

---
+++ app/Main2.java
@@ -0,0 +1,33 @@
+/**
+ * Main class of the Java program.
+ */
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println ("APARTADO1");
+        
+        for (int i = 7; i <= 77; i = i + 7){
+            System.out.println (i);
+        
+        }   
+        
+        System.out.println ("APARTADO2");
+        
+        for (int i = 2; i<= 20; i = i + 3) {
+            System.out.println (i);
+        } 
+        
+        
+        System.out.println ("APARTADO3");
+        
+        for ( int i = 99; i >= 0 ; i = i - 11) {
+            System.out.println (i);
+        }
+        
+        // Add your code here
+        
+    }
+    
+}
